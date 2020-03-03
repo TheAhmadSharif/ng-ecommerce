70a380d32fba2b529065c3e8b6c93e8c2e9fe03c
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-03-03 15:30:50 +0600

    cart total fix

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,19 +9,33 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   products;
-  quantity:Array<number> = [];
+  totalAmount:any;
+  quantity:Array<number> = [1];
   constructor(public cartService: CartService) {
     
    }
 
   ngOnInit(): void {
     this.products = this.cartService.getItems();
+    this.totalAmount = 0;
+
+    var arr = this.products;
+    var total = 0;
+    for(var i in arr) { 
+      this.totalAmount += parseFloat(arr[i].product_price);
+    }
+
   }
 
   removeItem(product:number) {
     this.cartService.items.splice(product,1);
   }
 
+  addNumber(quantity, price) {
+  }
+  totalPrice(){
+    console.log(this.quantity);
+  }
 }
 
 
