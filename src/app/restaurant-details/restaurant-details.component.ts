de71049c5b8b7180deec99bb6c4ad015015726d2
theahmadsharif <theahmadsharif@gmail.com>
2020-07-06 09:59:16 +0600

    combine two data

--- src/app/restaurant-details/restaurant-details.component.ts
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -27,12 +27,49 @@ export class RestaurantDetailsComponent implements OnInit {
   ngOnInit(): void {
       // this.items = this.db.list('time_table').valueChanges();
 
+
+      this.items = [];
       this.db.database.ref("time_table").once("value", (response:any) => {
           let data = response.val();
 
-          console.log(data, 'data');
+          let list = [];
+          this.db.database.ref("restaurant_details").once("value", function(snapshot) {
+      
 
+
+
+               var childData;
+               var childKey;
+               let rlist = snapshot.val();
+
+
+
+              for(let key in data) {
+                       if (data[key]["shop_temp_closing"]) {
+                            list.push({
+                              "_id": key,
+                              "shop_temp_closing": data[key]["shop_temp_closing"],
+                              "rid": data[key]["shop_temp_closing"]["rid"],
+                              "restauranr_details": rlist["rid_" + data[key]["shop_temp_closing"]["rid"]]
+                            })
+                         }
+                    
+                }
+
+
+
+
+          });
+
+          this.items = list;
+          console.log(this.items, '32');
+
+
+          
       })
+
+
+
 
   }
 
--- src/app/site-layout/_state/product.reducers.ts
+++ src/app/site-layout/_state/product.reducers.ts
@@ -12,11 +12,11 @@ export const ProductListReducer = createReducer(initialState,
 export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
   return function(state, action) {
 
-    console.group(action.type);
+    /*console.group(action.type);
     console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state);
     console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action);
     console.log(`%c next state`, `color: #4CAF50; font-weight: bold`, state);
-    console.groupEnd();
+    console.groupEnd();*/
     
  
     return reducer(state, action);
